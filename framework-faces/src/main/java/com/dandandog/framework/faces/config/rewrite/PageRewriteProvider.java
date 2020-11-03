1e21ddd013ed101071ee8f053fe598ee5123f0b0
Johnny <704365036@com>
2020-11-03 18:28:22 +0800

    rewrite

--- framework-faces/src/main/java/com/dandandog/framework/faces/config/rewrite/PageRewriteProvider.java
+++ framework-faces/src/main/java/com/dandandog/framework/faces/config/rewrite/PageRewriteProvider.java
@@ -40,6 +40,8 @@ public class PageRewriteProvider extends HttpConfigurationProvider {
                 .addRule(Join.path("/{path}?{params}").to(StrUtil.addSuffixIfNot("/{path}", ".faces?{params}")))
                 .addRule(Join.path("/auth/{path}").to(StrUtil.addSuffixIfNot("/auth/{path}", ".faces")))
                 .addRule(Join.path("/auth/{path}?{params}").to(StrUtil.addSuffixIfNot("/auth/{path}", ".faces?{params}")))
+                .addRule(Join.path("/gift/{path}").to(StrUtil.addSuffixIfNot("/gift/{path}", ".faces")))
+                .addRule(Join.path("/gift/{path}?{params}").to(StrUtil.addSuffixIfNot("/gift/{path}", ".faces?{params}")))
 
                 ;
     }
