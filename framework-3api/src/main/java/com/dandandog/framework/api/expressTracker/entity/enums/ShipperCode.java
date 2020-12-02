03e2dd2495c74a0aaf77bf5886908f04290c00bd
Johnny酱 <704365036@qq.com>
2020-12-03 07:55:35 +0800

    修改 AuditableEntity

--- framework-3api/src/main/java/com/dandandog/framework/api/expressTracker/entity/enums/ShipperCode.java
+++ framework-3api/src/main/java/com/dandandog/framework/api/expressTracker/entity/enums/ShipperCode.java
@@ -6,25 +6,19 @@ package com.dandandog.framework.api.expressTracker.entity.enums;
 
 public enum ShipperCode {
 
-    /**
-     * 百世快递
-     */
+    SF("顺丰速运"),
     HTKY("百世快递"),
-
-    /**
-     * 申通
-     */
-    STO("申通"),
-
-    /**
-     * 天天快递
-     */
-    HHTT("申通"),
-
-    /**
-     * 圆通
-     */
-    YTO("圆通");
+    ZTO("中通快递"),
+    STO("申通快递"),
+    YTO("圆通速递"),
+    YD("韵达速递"),
+    YZPY("邮政快递包裹"),
+    EMS("EMS"),
+    HHTT("天天快递"),
+    JD("京东快递"),
+    UC("优速快递"),
+    DBL("德邦快递"),
+    ZJS("宅急送");
 
     private final String name;
 
