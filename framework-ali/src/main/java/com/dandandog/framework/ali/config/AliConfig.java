e423418409397e6c285eeaf9a464d26a0d951dca
dz147 <1920098158@qq.com>
2021-04-02 18:09:13 +0800

    支付宝SDK配置

--- framework-ali/src/main/java/com/dandandog/framework/ali/config/AliConfig.java
+++ framework-ali/src/main/java/com/dandandog/framework/ali/config/AliConfig.java
@@ -5,6 +5,7 @@ import com.alipay.easysdk.kernel.Config;
 import com.dandandog.framework.ali.config.properties.AliProperties;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -18,25 +19,26 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @EnableConfigurationProperties(AliProperties.class)
 @ConditionalOnProperty(prefix = "fast-code.ali", value = {"enabled"}, matchIfMissing = true)
-public class AliConfig {
+public class AliConfig implements InitializingBean {
     private final AliProperties properties;
-
-    {
-        Factory.setOptions(getOptions());
-    }
 
     private Config getOptions() {
         Config config = new Config();
         config.protocol = properties.getProtocol();
         config.gatewayHost = properties.getGatewayHost();
         config.signType = properties.getSignType();
-        config.appId = properties.getProtocol();
+        config.appId = properties.getAppId();
         config.merchantPrivateKey = properties.getMerchantPrivateKey();
         config.merchantCertPath = properties.getMerchantCertPath();
         config.alipayCertPath = properties.getAlipayCertPath();
         config.alipayRootCertPath = properties.getAlipayRootCertPath();
+        config.encryptKey = properties.getEncryptKey();
         log.info(config.toString());
-        Factory.setOptions(config);
         return config;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Factory.setOptions(getOptions());
     }
 }
