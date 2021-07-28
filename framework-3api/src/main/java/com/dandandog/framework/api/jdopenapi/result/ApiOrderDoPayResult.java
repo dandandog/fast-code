11a94735050ef5ce5f78f8193fd53a5d136f648c
dz147 <1920098158@qq.com>
2021-07-28 14:52:52 +0800

    update

---
+++ framework-3api/src/main/java/com/dandandog/framework/api/jdopenapi/result/ApiOrderDoPayResult.java
@@ -0,0 +1,15 @@
+package com.dandandog.framework.api.jdopenapi.result;
+
+import com.dandandog.framework.api.jdopenapi.entity.AbstractApiResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Author: StephenZhang
+ * @date: 2021-07-23 10:24
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ApiOrderDoPayResult extends AbstractApiResponse {
+    private Boolean result;
+}
