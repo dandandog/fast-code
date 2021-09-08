080a53e1145e58ed58b7970cb007d90cf39037e3
JohnnyLiu <704365036@qq.com>
2021-09-08 18:22:38 +0800

    更新

--- framework-faces/src/main/java/com/dandandog/framework/faces/config/properties/PageProperties.java
+++ framework-faces/src/main/java/com/dandandog/framework/faces/config/properties/PageProperties.java
@@ -1,8 +1,11 @@
 package com.dandandog.framework.faces.config.properties;
 
 import com.dandandog.framework.common.config.constant.FastCodeConstant;
+import com.google.common.collect.Lists;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
 
 @Data
 @ConfigurationProperties(prefix = "fast-code.page")
@@ -22,7 +25,8 @@ public class PageProperties {
 
     private String forget = FastCodeConstant.FORGET_PAGE + FastCodeConstant.PAGE_SUFFIX;
 
-    private String suffix = FastCodeConstant.PAGE_SUFFIX;
+    private String[] anonymous = new String[] {login, loginFailed, index, error, access, notFound, forget};
 
+    private String suffix = FastCodeConstant.PAGE_SUFFIX;
 
 }
